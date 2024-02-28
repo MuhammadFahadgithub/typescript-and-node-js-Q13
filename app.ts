bfee8dfd74ee4b0f779add44008a57ac2bc77e40
MuhammadFahadgithub <159857192+MuhammadFahadgithub@users.noreply.github.com>
2024-02-28 16:36:21 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,10 @@
+//Task 13
+//Think of your favorite mode of transportation, such as motorcycle or
+// a car, and make a list that stores several examples.
+//Use your list to print a series of statments about these items,
+//such as "I would like to own a Honda motorcycle."
+
+let transportation : string[]=['car','bus','motorcycle','aeroplane']
+for(let i=0; i<transportation.length; i++){
+    console.log('I would like to own a '+ transportation[i])
+}
